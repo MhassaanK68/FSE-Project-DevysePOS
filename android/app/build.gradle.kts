1829793637bd3320d42e52f66e113d8ead3afd9a
Hassan <sufyansohail025@gmail.com>
2026-03-24 21:58:24 +0500

    project configuration changes to fix build errors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,8 @@ plugins {
 
 android {
     namespace = "com.devyse.devyse_pos"
-    compileSdk = flutter.compileSdkVersion
+    // Use 36 locally: Platform 35 is missing and auto-install fails (Windows unzip FileAlreadyExistsException in SDK .temp).
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -25,7 +26,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
